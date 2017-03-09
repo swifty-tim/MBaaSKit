62d05c482d0da4d952319dbeab24652d1092dffb
Timothy Barnard <timothy.barnard@mydit.ie>
2017-03-09 18:33:47 +0000

    version 0.2.8.9

---
+++ MBaaSKit/Classes/RCViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RCViewController.swift
+//  Pods
+//
+//  Created by Timothy Barnard on 09/03/2017.
+//
+//
+
+import Foundation
+
+public class RCViewController: UIViewController, ViewControllerLoad {
+    
+}
---
+++ MBaaSKit/Classes/ViewControllerLoad.swift
@@ -0,0 +1,52 @@
+//
+//  ViewControllerLoad.swift
+//  Pods
+//
+//  Created by Timothy Barnard on 09/03/2017.
+//
+//
+
+import Foundation
+
+public protocol ViewControllerLoad { }
+
+public extension ViewControllerLoad where Self: UIViewController {
+    
+    /**
+     setupTableViewController
+     - parameters:
+     - className: put self
+     - name: the name of the object instance
+     
+     */
+    public func setupTableViewController( className: UIViewController, _ name: String = "" ) {
+        
+        self.setup(className: String(describing: type(of: className)), tagValue: name)
+    }
+    
+    private func setup( className: String, tagValue : String ) {
+        
+        let dict = RCConfigManager.getClassProperties(className: className)
+        
+        for (key, _) in dict {
+            
+            switch key {
+            case "title" where dict.tryConvert(forKey: key) != "":
+                self.title =  dict.tryConvert(forKey: key)
+                break
+            case "backgroundColor" where dict.tryConvert(forKey: key) != "":
+                self.view.backgroundColor = RCFileManager.readJSONColor(keyVal:  dict.tryConvert(forKey: key) )
+                break
+            case "isUserInteractionEnabled" where dict.tryConvert(forKey: key) != "":
+                self.view.isUserInteractionEnabled = dict.tryConvert(forKey: key)
+                break
+            case "isHidden" where dict.tryConvert(forKey: key) != "":
+                self.view.isHidden = dict.tryConvert(forKey: key)
+                break
+                
+            default: break
+            }
+        }
+    }
+    
+}
